c98d25da286b4ac26777e0a05b4329c5f6db8883
catalinariquelme <catalina.riquelme.z@usach.cl>
2021-06-11 03:04:38 -0400

    salida vacunacionCompleta FUNCIONAL
    Salida vacunaciónCimpleta funciona, falta pulir detalles

--- Vacunados.c
+++ Vacunados.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "vacunados1D.h"
 #include "vacunados2D.h"
 #include "vacunas.h"
+
+time_t mktime(struct tm *timeptr);
 
 int* convertirFecha(char fecha[80]){
   //printf("Fecha %s\n",fecha);
@@ -30,7 +33,34 @@ int* convertirFecha(char fecha[80]){
   //printf("\n\n");
   return fechaSalida;
 }
+/*
+Entradas: date fecha(fecha inicial)
+Salida: date fecha(fecha tras determinado tiempo)
+Objetivo: calcular la fecha tras n semanas
+*/
+/*
+int proximaVacuna(int *fecha,int semanas){
+   int ret;
+   struct tm info;
+   char buffer[80];
 
+   info.tm_year =  - 1900;
+   info.tm_mon = 0;
+   info.tm_mday = 0 + (semanas*7);
+   info.tm_hour = 0;
+   info.tm_min = 0;
+   info.tm_sec = 0;
+   info.tm_isdst = 0;
+
+   ret = mktime(&info);
+   if( ret == -1 ) {
+      printf("Error: mktime\n");
+   } else {
+      strftime(buffer, sizeof(buffer), "%c", &info );
+      return(buffer);
+   }
+}
+*/
 //Entardas: char*nombre(nombre del archivo a leer)
 //Salida: int dimension(tamaño del archivo)
 //Objetivo: leer y almacenar la primera línea del archivo (dimension)
@@ -237,12 +267,19 @@ void ordenAbecedarioApellido(struct nodo1D *inicio){
   while (swapped);
 }
 
-void salidaVacunacionCompleta(nodo2D *inicio){
+
+
+
+/*
+Entradas: nodo2D *inicio(cabeza lista vacunados con dos dosis), nodo1D *inicio(cabeza lista vacunados con una dosis)
+Salida: -
+Objetivo: escribir .out que contenga las personas que tengan una vacunación completa
+*/
+void salidaVacunacionCompleta(nodo2D *inicio, nodo1D *inicio1D){
     FILE* arch;
     arch = fopen("vacunacionCompleta.out","w");
     if (!esListaVacia2D(inicio)){
       nodo2D* auxiliar=inicio;
-
       while (auxiliar!=NULL){
         fputs(auxiliar->vacunados2D.rut,arch);
         fputs(" ",arch);
@@ -256,6 +293,30 @@ void salidaVacunacionCompleta(nodo2D *inicio){
         fputs(" ",arch);
         fputs(auxiliar->vacunados2D.idVacuna,arch);
         fputs("\n",arch);
+
+        nodo1D* inicio2=inicio1D;
+        nodo2D* persona=auxiliar;
+        if (!esListaVacia1D(inicio1D)){
+          nodo1D* auxiliar2=inicio1D;
+          while (auxiliar2!=NULL){
+            if (strcmp(auxiliar2->vacunados1D.rut,persona->vacunados2D.rut)== 0){
+            fputs(auxiliar2->vacunados1D.rut,arch);
+            fputs(" ",arch);
+            fputs(auxiliar2->vacunados1D.nombre,arch);
+            fputs(" ",arch);
+            fputs(auxiliar2->vacunados1D.apellido,arch);
+            fputs(" ",arch);
+            fputs(auxiliar2->vacunados1D.edad,arch);
+            fputs(" ",arch);
+            fputs(auxiliar2->vacunados1D.fecha1dosis,arch);
+            fputs(" ",arch);
+            fputs(auxiliar2->vacunados1D.idVacuna,arch);
+            fputs("\n",arch);
+            break;
+            }
+            auxiliar2=auxiliar2->siguiente;
+          }
+        }
         auxiliar=auxiliar->siguiente;
       }
     fclose(arch);
@@ -286,7 +347,7 @@ int main(){
   recorrerLista1D(vacunados1D);
   //bubbleSort(vacunados1D);
   printf("--------------------------------------------------------------------------------------\n");
-  salidaVacunacionCompleta(vacunados2D);
+  salidaVacunacionCompleta(vacunados2D,vacunados1D);
   printf("escribi");
   //recorrerLista1D(vacunados1D);
   //ordenAbecedarioNombre(vacunados1D);
